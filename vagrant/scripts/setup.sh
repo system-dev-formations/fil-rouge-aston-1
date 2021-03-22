3005458cab588567fca089863f106acc757a26fd
dhaeseq <quentin.d'haese@social.aston-ecole.com>
2021-03-22 17:27:47 +0100

    add checks in vagrant provisionning

--- vagrant/scripts/setup.sh
+++ vagrant/scripts/setup.sh
@@ -3,97 +3,183 @@
 GREEN='\e[32m'
 NC='\033[0m'
 
+checkInstall() {
+  if ! command --version "$1" &> /dev/null
+  then
+      return 1
+  fi
+  return 0
+}
+
 echo -e "${GREEN}Provisioning virtual machine...${NC}"
+
+# Suppression d'un fichier Mint qui empeche d'installer snap
+if [[ -f /etc/apt/preferences.d/nosnap.pref ]]
+then
+  sudo rm -rf /etc/apt/preferences.d/nosnap.pref
+fi
 
 # Update
 echo -e "${GREEN}Updating${NC}"
-sudo apt-get update
-
-# Suppression d'un fichier Mint qui empeche d'installer snap
-sudo rm -rf /etc/apt/preferences.d/nosnap.pref
-
 sudo apt update
 
-sudo apt-get -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+# Basic packages
+if ! [[ -d /usr/share/doc/apt-transport-https/ ]]
+then
+  echo -e "${GREEN}Installing apt-transport-https${NC}"
+  sudo apt-get -y install apt-transport-https
+fi
+if ! [[ -d /etc/ca-certificates/ ]]
+then
+  echo -e "${GREEN}Installing ca-certificates${NC}"
+  sudo apt-get -y install ca-certificates
+fi
+if checkInstall curl
+then
+  echo -e "${GREEN}Installing curl${NC}"
+  sudo apt-get -y install curl
+fi
+if ! [[ -d /usr/share/doc/gnupg-agent/ ]]
+then
+  echo -e "${GREEN}Installing gnupg-agent${NC}"
+  sudo apt-get -y install gnupg-agent
+fi
+if ! [[ -d /usr/share/doc/software-properties-common/ ]]
+then
+  echo -e "${GREEN}Installing software-properties-common${NC}"
+  sudo apt-get -y install software-properties-common
+fi
 
 # SNAP
-echo -e "${GREEN}Installing Snap${NC}"
-sudo apt install snapd
+if checkInstall snap
+then
+  echo -e "${GREEN}Installing Snap${NC}"
+  sudo apt install snapd
+fi
 
 # GIT
-echo -e "${GREEN}Installing Git${NC}"
-sudo add-apt-repository -y ppa:git-core/ppa
-sudo apt update
-sudo apt install git -y
+if checkInstall git
+then
+  echo -e "${GREEN}Installing Git${NC}"
+  sudo add-apt-repository -y ppa:git-core/ppa
+  sudo apt update
+  sudo apt install git -y
+fi
 
 # JAVA
-sudo add-apt-repository -y ppa:openjdk-r/ppa
-sudo apt-get update
-sudo apt install openjdk-11-jdk -y
+if checkInstall java
+then
+  echo -e "${GREEN}Installing Java${NC}"
+  sudo add-apt-repository -y ppa:openjdk-r/ppa
+  sudo apt-get update
+  sudo apt install openjdk-11-jdk -y
+fi
 
 # INTELLIJ
-echo -e "${GREEN}Installing IntelliJ${NC}"
-sudo snap install intellij-idea-community --classic
+
+if ! [[ -d /snap/intellij-idea-community ]]
+then
+  echo -e "${GREEN}Installing IntelliJ${NC}"
+  sudo snap install intellij-idea-community --classic
+fi
 
 # WGET
-echo -e "${GREEN}Installing Wget${NC}"
-sudo apt-get -y install wget
+if checkInstall wget
+then
+  echo -e "${GREEN}Installing Wget${NC}"
+  sudo apt-get -y install wget
+fi
 
 # LOMBOK
-echo -e "${GREEN}Installing lombok${NC}"
-wget https://projectlombok.org/downloads/lombok.jar
+if ! [[ -f /home/vagrant/lombok.jar ]]
+then
+  echo -e "${GREEN}Installing lombok${NC}"
+  wget https://projectlombok.org/downloads/lombok.jar -q
+fi
 
 # VSCODE
-echo -e "${GREEN}Installing Visual Studio Code${NC}"
-sudo snap install code --classic
+
+if checkInstall code
+then
+  echo -e "${GREEN}Installing Visual Studio Code${NC}"
+  sudo snap install code --classic
+fi
 
 # OHMYZSH
-sudo apt-get -y install zsh
-sudo chsh -s /usr/bin/zsh "$USER"
-wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
-cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
-source ~/.zshrc
+if checkInstall zsh
+then
+  echo -e "${GREEN}Installing OhMyZsh${NC}"
+  sudo apt-get -y install zsh
+  sudo chsh -s /usr/bin/zsh "$USER"
+  wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
+  cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+  source ~/.zshrc
+fi
 
 # DOCKER
-echo -e "${GREEN}Installing Docker${NC}"
-# Before installing Docker Engine for the first time on a new host machine,
-# you need to set up the Docker repository
-# Add Docker’s official GPG key:
-curl -L https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(. /etc/os-release; echo "$UBUNTU_CODENAME") stable"
-sudo apt-get update
-sudo apt-get -y install docker-ce docker-ce-cli containerd.io
-sudo usermod -aG docker "$USER"
-newgrp docker
+if checkInstall docker
+then
+  echo -e "${GREEN}Installing Docker${NC}"
+  # Before installing Docker Engine for the first time on a new host machine,
+  # you need to set up the Docker repository
+  # Add Docker’s official GPG key:
+  curl -L https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+  sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(. /etc/os-release; echo "$UBUNTU_CODENAME") stable"
+  sudo apt-get update
+  sudo apt-get -y install docker-ce docker-ce-cli containerd.io
+  sudo usermod -aG docker "$USER"
+  newgrp docker
+fi
 
 # BRAVE
-echo -e "${GREEN}Installing Brave Browser${NC}"
-sudo snap install brave
+if ! [[ -d /snap/brave/ ]]
+then
+  echo -e "${GREEN}Installing Brave Browser${NC}"
+  sudo snap install brave
+fi
 
 # CHROME
-echo -e "${GREEN}Installing Chrome Browser${NC}"
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt install ./google-chrome-stable_current_amd64.deb -y
+if checkInstall google-chrome
+then
+  echo -e "${GREEN}Installing Chrome Browser${NC}"
+  wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -q
+  sudo apt install ./google-chrome-stable_current_amd64.deb -y
+fi
 
 # DBEAVER
-echo -e "${GREEN}Installing DBeaver${NC}"
-sudo snap install dbeaver-ce
+if ! [[ -d /snap/dbeaver-ce/ ]]
+then
+  echo -e "${GREEN}Installing DBeaver${NC}"
+  sudo snap install dbeaver-ce
+fi
 
 # POSTGRES
-echo -e "${GREEN}Installing PostgreSQL${NC}"
-sudo apt install postgresql postgresql-contrib -y
+if checkInstall psql
+then
+  echo -e "${GREEN}Installing PostgreSQL${NC}"
+  sudo apt install postgresql postgresql-contrib -y
+fi
 
 # POSTMAN
-echo -e "${GREEN}Installing Postman${NC}"
-sudo snap install postman
+if ! [[ -d /snap/postman/ ]]
+then
+  echo -e "${GREEN}Installing Postman${NC}"
+  sudo snap install postman
+fi
 
 # ANGULAR
-echo -e "${GREEN}Installing Angular${NC}"
-curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
-source ~/.bashrc
-nvm install node
-npm install -g @angular/cli
+if checkInstall ng
+then
+  echo -e "${GREEN}Installing Angular${NC}"
+  curl -L https://deb.nodesource.com/setup_14.x | bash
+  sudo apt install nodejs -y
+  npm install npm@latest -g
+  npm install -g @angular/10
+fi
 
 # ANSIBLE
-echo -e "${GREEN}Installing Ansible${NC}"
-sudo apt install ansible -y
+if checkInstall ansible
+then
+  echo -e "${GREEN}Installing Ansible${NC}"
+  sudo apt install ansible -y
+fi
